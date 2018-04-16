39ffcc37f69c022f51cb3421fe2d4401ad174d8f
tkc <tkc@lemona.de>
2018-04-16 12:55:13 +0900

    feat: Add InjectableIntentService based on InjectableService

---
+++ src/main/java/de/lemona/android/guice/InjectableIntentService.java
@@ -0,0 +1,30 @@
+package de.lemona.android.guice;
+
+import android.app.IntentService;
+
+import com.google.inject.Binder;
+import com.google.inject.Module;
+
+public abstract class InjectableIntentService extends IntentService {
+    /**
+     * {@inheritDoc}
+     */
+    public InjectableIntentService(String name) {
+        super(name);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Injection.createInjector(this, new Module() {
+            @Override
+            public void configure(Binder binder) {
+                onInject(binder);
+            }
+        });
+    }
+
+    protected void onInject(@SuppressWarnings("unused") Binder binder) {
+        // Allow classes to override this method
+    }
+}
